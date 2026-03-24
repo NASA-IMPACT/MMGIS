7386672057a1aeb9b9b9f085c6b0df8de1bc5c14
Sandesh Pandey <sandeshsinbox@gmail.com>
2026-03-24 15:41:13 -0500

    Initial implementation of enhancedLayout flag to switch between default and enhanced layout

--- src/essence/Basics/UserInterface_/UserInterface_.js
+++ src/essence/Basics/UserInterface_/UserInterface_.js
@@ -1,18 +1,30 @@
-let UserInterface = null
-// Check useragent for now (FIXME use a better way to determine if mobile or not)
-// https://gist.github.com/dalethedeveloper/1503252
-let isMobile = window.navigator.userAgent.match(
-    /Mobi|iP(hone|od|ad)|Android|BlackBerry/
-)
-if (isMobile) {
-    UserInterface = () =>
-        import('./UserInterfaceMobile_').then((module) => {
-            module.default.isMobile = true
-            return module.default
-        })
-} else {
-    UserInterface = () =>
-        import('./UserInterfaceDefault_').then((module) => module.default)
+// Function to load the appropriate UserInterface based on config
+async function loadUserInterface(config = null) {
+    // Check useragent for now (FIXME use a better way to determine if mobile or not)
+    // https://gist.github.com/dalethedeveloper/1503252
+    let isMobile = window.navigator.userAgent.match(
+        /Mobi|iP(hone|od|ad)|Android|BlackBerry/
+    )
+
+    // Check for enhanced layout from config or environment variable
+    let useEnhancedLayout = false
+    if (config?.panels?.enhancedLayout === true) {
+        useEnhancedLayout = true
+    } else if (window.mmgisglobal?.ENABLE_ENHANCED_LAYOUT === 'true') {
+        useEnhancedLayout = true
+    }
+
+    if (isMobile) {
+        const module = await import('./UserInterfaceMobile_')
+        module.default.isMobile = true
+        return module.default
+    } else if (useEnhancedLayout) {
+        const module = await import('./UserInterfaceEnhanced_')
+        return module.default
+    } else {
+        const module = await import('./UserInterfaceDefault_')
+        return module.default
+    }
 }
 
-export default await UserInterface
+export default loadUserInterface
