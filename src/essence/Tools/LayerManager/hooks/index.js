be5d9181247e7439896b3d3871c4f75ed2feb360
Slesa Adhikari <slesaad@gmail.com>
2026-05-18 14:47:27 -0500

    refactor(layermanager): remove legacy components/, hooks/, utils/, and LayerManagerTool.css

--- src/essence/Tools/LayerManager/hooks/index.js
+++
@@ -1,3 +0,0 @@
-export { default as useColormapGradient } from './useColormapGradient'
-export { default as useClickOutside } from './useClickOutside'
-export { default as useAvailableColormaps } from './useAvailableColormaps'
