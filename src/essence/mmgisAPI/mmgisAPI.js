be389cef69b40a2daf65505ff0f79700127cc466
Sandesh Pandey <sandeshsinbox@gmail.com>
2026-06-02 10:03:12 -0500

    feat: add getRawConfigData method to get original Layer configuration

--- src/essence/mmgisAPI/mmgisAPI.js
+++ src/essence/mmgisAPI/mmgisAPI.js
@@ -357,6 +357,9 @@ var mmgisAPI_ = {
             return matchedLayers
         } else return L_.layers.data
     },
+    getRawConfigData: function () {
+        return L_.configData
+    },
     getLayers: function () {
         return L_.layers.layer
     },
@@ -693,6 +696,10 @@ var mmgisAPI = {
      * @returns {object} - an object containing the visibility state of each layer
      */
     getLayerConfigs: mmgisAPI_.getLayerConfigs,
+    /** getRawConfigData - returns the original unmutated configuration data 
+     * @returns {object} - the original configData object
+     */
+    getRawConfigData: mmgisAPI_.getRawConfigData,
     /** getLayers - returns an object with the visibility state of all layers
      * @returns {object} - an object containing the visibility state of each layer
      */
